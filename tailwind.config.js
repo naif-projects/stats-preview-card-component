62c13169aff6303205c4c9e497b67b58f5253999
Naif Sameer <42966227+naif-sameer@users.noreply.github.com>
2022-06-06 07:54:33 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-light': 'hsl(215, 32%, 27%)',
+        'primary-base': 'hsl(216, 50%, 16%)',
+        'primary-dark': 'hsl(217, 54%, 11%)',
+
+        'secondary-light': 'hsl(215, 51%, 70%)',
+        'secondary-base': 'hsl(178, 100%, 50%)',
+      },
+
+      fontFamily: {
+        primary: ["'Outfit'", 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
